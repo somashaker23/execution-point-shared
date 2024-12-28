c29a9d56ea4e908d62d6e07e4276b8e0a9ad7d92
Soma Sekhar <>
2024-12-28 17:10:27 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="job-interface-shared",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    description="Shared library for JobInterface and related utilities.",
+    author="Soma Sekhar",
+    author_email="somashaker23@gmail.com",
+    url="https://github.com/somashaker23/execution-point-shared",
+)
